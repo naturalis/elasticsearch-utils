05bdd33ac8f6a4506cb9a0ef625a4345c2c7299a
Ayco Holleman <ayco.holleman@naturalis.nl>
2019-10-16 10:07:31 +0200

    Copied from CoL+. Lots of compile errors!

---
+++ src/main/java/org/col/es/IndexConfig.java
@@ -0,0 +1,8 @@
+package org.col.es;
+
+public class IndexConfig {
+  
+  public int numShards = 1;
+  public int numReplicas = 0; 
+  
+}
---
+++ src/main/java/org/col/es/response/Bucket.java
@@ -0,0 +1,22 @@
+package org.col.es.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * The Elasticsearch response object corresponding to the {@link FacetValue} class.
+ */
+public class Bucket {
+
+  private Object key;
+  @JsonProperty("doc_count")
+  private int docCount;
+
+  public Object getKey() {
+    return key;
+  }
+
+  public int getDocCount() {
+    return docCount;
+  }
+
+}
