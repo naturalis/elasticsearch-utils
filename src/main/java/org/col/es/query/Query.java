b61cada744093da199d5f10d72a0b80b52a3494b
Ayco Holleman <ayco.holleman@naturalis.nl>
2020-01-22 08:47:45 +0100

    changed artifict name

--- src/main/java/org/col/es/query/Query.java
+++ src/main/java/org/col/es/query/Query.java
@@ -1,27 +1,27 @@
 package org.col.es.query;
 
 /**
- * Represents the "query" part of an Elasticsearch search quest. Since queries can take very different forms, the only
- * aspects of a query this interface defines are its name and its boost value.
+ * Represents the "query" part of an Elasticsearch search quest. Since queries can take very different forms, the only aspects of a query
+ * this interface defines are its name and its boost value.
  */
 public interface Query {
 
   /**
-   * Turns the query into a named query. Fluent interface. Named queries are especially useful with compound queries like
-   * {@link BoolQuery}. If you name the constraints within a {@code BoolQuery}, the Elasticsearch response will tell you
-   * which of those constraints were satisfied for each document in the result set.
+   * Turns the query into a named query. Fluent interface (returns the instance on which it is called). Named queries are especially useful
+   * with compound queries like {@link BoolQuery}: if you name the constraints within a <code>BoolQuery</code>, Elasticsearch will tell you
+   * for each document in the query result which constraints it satisfied.
    * 
    * @param name
    * @return
    */
-  public <Q extends Query> Q withName(String name);
+  <Q extends Query> Q withName(String name);
 
   /**
-   * Sets a boost value for documents matching this query. Fluent interface.
+   * Sets a boost value for documents matching this query. Fluent interface (returns the instance on which it is called).
    * 
    * @param boost
    * @return
    */
-  public <Q extends Query> Q withBoost(Double boost);
+  <Q extends Query> Q withBoost(Double boost);
 
 }
